9699d5b5d9b27b5b901de1c184438647cac62491
awnicholls <awnicholls56@gmail.com>
2021-08-11 17:26:20 +0100

    added cart and cart styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import logo from '../../assets/logo.png';
-import useStyles from'./styles';
-const Navbar = () => {
-    const classes = useStyles();
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/logo.png";
+import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
 
-    return (
-        <>
-            <AppBar position='fixed' className={classes.appBar} color='inherit'>
-<Toolbar>
-<Typography varient='h6' className={classes.title} color='inherit'>
-    <img src={logo} alt='Soy Natural by Hannah' height='50px' className={classes.image}/>
-    Soy Natural by Hannah
-</Typography>
-<div className={classes.grow}/>
-<div className={classes.button}>
-    <IconButton aria-label='Show cart items' color='inherit'>
-        <Badge badgeContent={2} color='secondary'> 
-        <ShoppingCart />
-        </Badge>
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
 
-    </IconButton>
-</div>
-</Toolbar>
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            varient="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Soy Natural by Hannah"
+              height="50px"
+              className={classes.image}
+            />
+            Soy Natural by Hannah
+          </Typography>
+          <div className={classes.grow} />
+          {!(location.pathname === '/cart') && (
+          <div className={classes.button}>
+            {/* <link to='/cart'>go to cart</link> */}
+            <IconButton
+              component={Link}
+              to="/cart"
+              aria-label="Show cart items"
+              color="inherit"
+            >
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-            </AppBar>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
