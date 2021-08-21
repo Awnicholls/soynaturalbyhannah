94ec9c98ee43b0facfd2ed57f2b8374a54c7cca7
Awnicholls <awnicholls56@gmail.com>
2021-08-21 20:48:35 +0100

    changed mobile scaling

--- src/components/Homepage/Hero/styles.js
+++ src/components/Homepage/Hero/styles.js
@@ -3,5 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   hero:{
       height: '80vh',
+      [theme.breakpoints.down("md")]: {
+        height: '50vh',
+      },
   },
 }));
